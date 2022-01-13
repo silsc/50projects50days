85d11b0b1d4eb334295041890276652134f6392d
Restyled.io <commits@restyled.io>
2022-01-13 21:58:53 +0000

    Restyled by prettier

--- 39-Password_Strength_Background/script.js
+++ 39-Password_Strength_Background/script.js
@@ -1,7 +1,7 @@
-const passwordEl = document.getElementById('password');
-const backgroundEl = document.getElementById('background');
+const passwordEl = document.getElementById("password");
+const backgroundEl = document.getElementById("background");
 
-passwordEl.addEventListener('input', (e) => {
+passwordEl.addEventListener("input", (e) => {
   const input = e.target.value;
   const length = input.length;
   const blurValue = 20 - length * 2;

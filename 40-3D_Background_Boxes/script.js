38f74f4b28f1c16c6d4900d8643075abe6cc2af9
Restyled.io <commits@restyled.io>
2022-01-17 14:58:15 +0000

    Restyled by clang-format

--- 40-3D_Background_Boxes/script.js
+++ 40-3D_Background_Boxes/script.js
@@ -2,11 +2,11 @@ const boxesEl = document.getElementById('boxes');
 const btnEl = document.getElementById('btn');
 
 function createBoxes() {
-  for(let i = 0; i < 4; i++ ) {
-    for(let j = 0; j < 4; j++) {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
       const box = document.createElement('div');
       box.classList.add('box');
-      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
+      box.style.backgroundPosition = `${- j * 125}px ${- i * 125}px`
       boxesEl.appendChild(box);
     }
   }
@@ -14,6 +14,4 @@ function createBoxes() {
 
 createBoxes()
 
-btnEl.addEventListener('click', () => {
-  boxesEl.classList.toggle('big');
-})
+btnEl.addEventListener('click', () => { boxesEl.classList.toggle('big'); })

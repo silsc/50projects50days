0c1d38377678ea14a6c5741293777be6d039d66c
Silvia Salat <salat.silvia@gmail.com>
2022-02-01 10:42:44 +0100

    FINISHED(42) exercise completed

---
+++ 42-Live_User_Filter/script.js
@@ -0,0 +1,42 @@
+const resultEl = document.getElementById('result')
+const filterEL = document.getElementById('filter')
+const listItems = [];
+
+getData()
+
+
+filterEL.addEventListener('input', (e) => {
+  filterData(e.target.value)
+})
+
+async function getData(){
+  const res = await fetch('https://randomuser.me/api/?results=50')
+  const {results} = await res.json()
+
+  // CLear the results
+  resultEl.innerHTML = ''
+
+  results.forEach(user => {
+    const li = document.createElement('li')
+    listItems.push(li)
+    li.innerHTML = `
+      <img src="${user.picture.large}" alt="${user.name.first}">
+      <div class="user-info">
+            <h4>${user.name.first} ${user.name.last}</h4>
+            <p>${user.location.city}, ${user.location.country}</p>
+      </div>
+    `
+
+    resultEl.appendChild(li)
+  })
+}
+
+function filterData(searchTerm) {
+  listItems.forEach(item => {
+    if(item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+      item.classList.remove('hide')
+    } else {
+      item.classList.add('hide')
+    }
+  })
+}

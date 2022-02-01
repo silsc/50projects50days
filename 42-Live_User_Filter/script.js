a76986e38141987f9a9c971fc0431f363fb831ba
Restyled.io <commits@restyled.io>
2022-02-01 09:43:39 +0000

    Restyled by prettier

--- 42-Live_User_Filter/script.js
+++ 42-Live_User_Filter/script.js
@@ -1,39 +1,41 @@
-const resultEl = document.getElementById('result')
-const filterEL = document.getElementById('filter')
+const resultEl = document.getElementById("result");
+const filterEL = document.getElementById("filter");
 const listItems = [];
 
-getData()
+getData();
 
-filterEL.addEventListener('input', (e) => {filterData(e.target.value)})
+filterEL.addEventListener("input", (e) => {
+  filterData(e.target.value);
+});
 
 async function getData() {
-  const res = await fetch('https://randomuser.me/api/?results=50')
-  const {results} = await res.json()
+  const res = await fetch("https://randomuser.me/api/?results=50");
+  const { results } = await res.json();
 
   // CLear the results
-  resultEl.innerHTML = ''
+  resultEl.innerHTML = "";
 
-  results.forEach(user => {
-    const li = document.createElement('li')
-  listItems.push(li)
-  li.innerHTML = `
+  results.forEach((user) => {
+    const li = document.createElement("li");
+    listItems.push(li);
+    li.innerHTML = `
       <img src="${user.picture.large}" alt="${user.name.first}">
       <div class="user-info">
             <h4>${user.name.first} ${user.name.last}</h4>
             <p>${user.location.city}, ${user.location.country}</p>
       </div>
-    `
+    `;
 
-    resultEl.appendChild(li)
-  })
+    resultEl.appendChild(li);
+  });
 }
 
 function filterData(searchTerm) {
-  listItems.forEach(item => {
+  listItems.forEach((item) => {
     if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
-      item.classList.remove('hide')
+      item.classList.remove("hide");
     } else {
-      item.classList.add('hide')
+      item.classList.add("hide");
     }
-  })
+  });
 }
